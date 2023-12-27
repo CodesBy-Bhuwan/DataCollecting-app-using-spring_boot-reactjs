c9c70aab791dc4bf22a42a18c071b05880d52915
CodesBy Bhuwan <b.coding817@gmail.com>
2023-12-27 21:17:48 +0545

    complete backend

--- system/src/main/java/com/learncodes/system/Controller/StudentController.java
+++ system/src/main/java/com/learncodes/system/Controller/StudentController.java
@@ -1,4 +1,28 @@
 package com.learncodes.system.Controller;
 
+
+import com.learncodes.system.Service.StudentService;
+import com.learncodes.system.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "new Student is added";
+    }
+    @GetMapping("/getAll")
+    public List<Student> getAllStudent(){
+        return studentService.getAllStudents();
+    }
+
+
 }
--- system/src/main/java/com/learncodes/system/Service/StudentService.java
+++ system/src/main/java/com/learncodes/system/Service/StudentService.java
@@ -1,4 +1,10 @@
 package com.learncodes.system.Service;
 
+import com.learncodes.system.model.Student;
+import jakarta.persistence.Entity;
+import org.hibernate.mapping.List;
+@Entity
 public interface StudentService {
+    public Student saveStudent(Student student);
+    public List<Student> getAllStudent();
 }
